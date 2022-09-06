41ea41e8ac92b487bc4486dcbdca7086aa242ad5
Julien_Raedler <jr@asperger-ag.ch>
2022-09-06 16:43:44 +0200

    controllers for category and trophy made

---
+++ app/Http/Controllers/TrophyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trophy;
+use Illuminate\Http\Request;
+
+class TrophyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Trophy  $trophy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trophy $trophy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Trophy  $trophy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trophy $trophy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Trophy  $trophy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trophy $trophy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Trophy  $trophy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trophy $trophy)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TrophyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('categories', CategoryController::class)->only([
+    'index', 'store', 'show', 'update', 'destroy'
+]);
+
+Route::resource('trophies', TrophyController::class)->only([
+    'index', 'store', 'show', 'update', 'destroy'
+]);
+
+Route::get('/trophies/sortByDate', [TrophyController::class, 'sortByDate']);
+Route::get('/trophies/sortByRank', [TrophyController::class, 'sortByRank']);
+
+Route::get('/trophies/filterByType/{type}', [TrophyController::class, 'filterByType']);
+Route::get('/trophies/filterByRank/{rank}', [TrophyController::class, 'filterByRank']);
+Route::get('/trophies/filterByCategory/{category}', [TrophyController::class, 'filterByCategory']);
